ef113783ffbc98dc5d473dc5eb2270854e55b692
mansiHaldankar <mansihaldankar15@gmail.com>
2020-02-21 08:05:29 +0530

    Side drawer animation and portal

--- src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import NavLinks from '../NavLinks/NavLinks';
 import MainHeader from '../MainHeader/MainHeader';
 import SideDrawers from '../SideDrawers/SideDrawers';
+import Backdrop from '../../UIElements/Backdrop/Backdrop';
 import './MainNavigation.css';
 
 
 const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
     return (
         <React.Fragment>
-            <SideDrawers className = 'main-navigation__drawer-nav'>
+            {drawerIsOpen && <Backdrop onClick = { closeDrawer }/> }
+            <SideDrawers className = 'main-navigation__drawer-nav' show = {drawerIsOpen} onClick = {closeDrawer}>
                 <NavLinks/>
             </SideDrawers>
             <MainHeader>
-                <button className = 'main-navigation__menu-btn'>
+                <button className = 'main-navigation__menu-btn' onClick = { openDrawer }>
                     <span />
                     <span />
                     <span />
